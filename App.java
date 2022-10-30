366919e44a2ee7eecc02d6fd0304c179004b6bc6
Nusret ŞAHAN <108969257+nusret-sahan@users.noreply.github.com>
2022-10-30 17:46:56 +0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,38 @@
+package csd;
+
+import java.util.Scanner;
+
+public class App {
+
+	public static void main(String[] args)
+	{
+		for (;;)
+			SortNumbers.run();
+	}
+
+}
+
+class SortNumbers {
+	public static void run()
+	{
+			Scanner kb = new Scanner(System.in);
+			System.out.println("Üç tane sayı giriniz:");
+			int a = kb.nextInt();
+			int b = kb.nextInt();
+			int c = kb.nextInt();
+			
+			int min = Math.min(Math.min(a, b), c);
+			int max = Math.max(Math.max(a, b), c);
+			int mid = a + b + c - min - max;
+			
+			if (min < mid && mid < max)
+				System.out.printf("%d < %d < %d%n",min, mid, max);
+			else if (min == mid && mid < max)
+				System.out.printf("%d = %d < %d%n",min, mid, max);
+			else if (min < mid && mid == max)
+				System.out.printf("%d < %d = %d%n",min, mid, max);
+			else  
+				System.out.printf("%d = %d = %d%n",min, mid, max);
+			
+	}
+}
