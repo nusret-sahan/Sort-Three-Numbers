80fe139f6060c962cc8b249139c1ca3e9c85afb2
Nusret ŞAHAN <108969257+nusret-sahan@users.noreply.github.com>
2022-11-01 21:19:00 +0300

    Update App.java

--- App.java
+++ App.java
@@ -15,24 +15,24 @@ public class App {
 class SortNumbers {
 	public static void run()
 	{
-			Scanner kb = new Scanner(System.in);
-			System.out.println("Üç tane sayı giriniz:");
-			int a = kb.nextInt();
-			int b = kb.nextInt();
-			int c = kb.nextInt();
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Üç tane sayı giriniz:");
+		int a = kb.nextInt();
+		int b = kb.nextInt();
+		int c = kb.nextInt();
 			
-			int min = Math.min(Math.min(a, b), c);
-			int max = Math.max(Math.max(a, b), c);
-			int mid = a + b + c - min - max;
+		int min = Math.min(Math.min(a, b), c);
+		int max = Math.max(Math.max(a, b), c);
+		int mid = a + b + c - min - max;
 			
-			if (min < mid && mid < max)
-				System.out.printf("%d < %d < %d%n",min, mid, max);
-			else if (min == mid && mid < max)
-				System.out.printf("%d = %d < %d%n",min, mid, max);
-			else if (min < mid && mid == max)
-				System.out.printf("%d < %d = %d%n",min, mid, max);
-			else  
-				System.out.printf("%d = %d = %d%n",min, mid, max);
+		if (min < mid && mid < max)
+			System.out.printf("%d < %d < %d%n",min, mid, max);
+		else if (min == mid && mid < max)
+			System.out.printf("%d = %d < %d%n",min, mid, max);
+		else if (min < mid && mid == max)
+			System.out.printf("%d < %d = %d%n",min, mid, max);
+		else  
+			System.out.printf("%d = %d = %d%n",min, mid, max);
 			
 	}
 }
